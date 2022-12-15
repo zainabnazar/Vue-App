490944b464a43e4455f318ea09276cc99aca1806
zainabnoori <zainab.noori@and.digital>
2022-12-15 13:30:44 +0000

    Add the delete request to the mock api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,27 @@ export default {
   name:"Home",
   components:{ photos },
   methods:{
-    removePhoto(id){
-     this.photos= this.photos.filter((photo)=> photo.id !==id);
+     async removePhoto(id){
+      const res = await fetch(`https://639b148631877e43d6818843.mockapi.io/photos/${id}`,
+       {
+         method: "DELETE",
+       });
+      if (res.status === 200){
+        this.photos= this.photos.filter((photo)=> photo.id !==id)
+      } else{
+      console.log("error")  
+      } 
     },
     async fetchPhotos(){;
       const res = await fetch("https://639b148631877e43d6818843.mockapi.io/photos")
       const data = await res.json();
+      console.log(data);
       return data;
     },
     async fetchPhoto(id){;
       const res = await fetch(`https://639b148631877e43d6818843.mockapi.io/photos/${id}`)
       const data = await res.json();
+      console.log(data);
       return data;
     }
   },
